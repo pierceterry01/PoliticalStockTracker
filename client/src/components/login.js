6fd3a060b22468a5d99eb6e5f12db6ab91f2d666
Pierce Terry <pierceeterry@gmail.com>
2024-10-17 15:54:50 -0500

    reformatted login and register pages to React

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,7 +1,38 @@
+import '../styles/styles.css';
 import React from 'react';
+import { Link } from 'react-router-dom';  
 
 function LoginPage() {
-    return <h1>Login</h1>
+    return (
+        <div className="login-body">
+            <Link to="/" className="back-arrow">&#8592;</Link> 
+            <header className="login-header">
+                <h1>Outsider Trading</h1>
+            </header>
+            <div className="sign-in-forms">
+                <h1>Login</h1>
+                <div className="header-line"></div>
+                <div className="login-form-content">
+                    <form>
+                        <div className="login-form-group">
+                            <label htmlFor="email" className="login-label">Email</label>
+                            <input className="login-input" type="text" id="email" name="email" />
+                        </div>
+                        <div className="login-form-group">
+                            <label htmlFor="password" className="login-label">Password</label>
+                            <input className="login-input" type="password" id="password" name="password" />
+                        </div>
+                        <div className="login-form-group">
+                            <input type="submit" value="Log in" className="login-button" />
+                        </div>
+                        <div className="login-form-group">
+                            <Link to="/register" className="create-account-link">Create an Account here</Link> {}
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default LoginPage
+export default LoginPage;
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,7 +1,42 @@
+import '../styles/styles.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
-    return <h1>Register</h1>
+    return (
+        <div className="create-account-body">
+            <Link to="/" className="back-arrow">&#8592;</Link> 
+            <header className="create-account-header">
+                <h1>Outsider Trading</h1>
+            </header>
+            <div className="create-account-forms">
+                <h1>Create Account</h1>
+                <div className="header-line"></div>
+                <div className="create-account-form-content">
+                    <form>
+                        <div className="create-account-form-group">
+                            <label htmlFor="username" className="create-account-label">Username</label>
+                            <input className="create-account-input" type="text" id="username" name="username" />
+                        </div>
+                        <div className="create-account-form-group">
+                            <label htmlFor="email" className="create-account-label">Email</label>
+                            <input className="create-account-input" type="text" id="email" name="email" />
+                        </div>
+                        <div className="create-account-form-group">
+                            <label htmlFor="password" className="create-account-label">Password</label>
+                            <input className="create-account-input" type="password" id="password" name="password" />
+                        </div>
+                        <div className="create-account-form-group">
+                            <input type="submit" value="Create Account" className="create-account-button" />
+                        </div>
+                        <div className="create-account-form-group">
+                            <Link to="/login" className="login-link">Sign in here</Link> 
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default RegisterPage
