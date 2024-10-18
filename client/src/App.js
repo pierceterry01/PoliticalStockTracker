5c99c3e2b07548ecee3313659a81e7e2cd70ae3c
Pierce Terry <pierceeterry@gmail.com>
2024-10-18 15:05:43 -0500

    Fixed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/landing-page';
+import LoginPage from './components/login';
+import RegisterPage from './components/register';
+import PortfolioPage from './components/portfolio';
+import StockViewPage from './components/stock-view';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/portfolio" element={<PortfolioPage />} />
+
+        <Route path="/stock-view" element={<StockViewPage />} />
+        
+
+      </Routes>
+    </Router>
   );
 }
 
