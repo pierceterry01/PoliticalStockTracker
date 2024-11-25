018517f60aca86fdfa0d2717dbbdc788edc9ce53
Pierce Terry <pierceeterry@gmail.com>
2024-11-25 09:42:07 -0600

    added chart files using chart.js

---
+++ client/src/charts/portfolioComposition.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Pie } from "react-chartjs-2"; 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function PortfolioCompositionChart({ data }) {
+  const totalStocks = data.reduce((total, item) => total + item.count, 0);
+
+  // chart data formatting
+  const chartData = {
+    labels: data.map((item) => item.symbol), 
+    datasets: [
+      {
+        label: "Portfolio Composition",
+        data: data.map((item) => item.count), 
+        backgroundColor: [
+          "#893fe2",
+          "#ec3aa6",
+          "#fee131",
+          "#12adf9",
+          "#20f6c2",
+        ],
+        borderWidth: 0,
+      }
+    ]
+  };
+  const options = {
+    plugins: {
+      legend: {
+        display: true, 
+        position: "right", 
+        labels: {
+          color: "white", 
+          boxWidth: 15, 
+          padding: 10, 
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: function(tooltipItem) {
+            const percentage = ((tooltipItem.raw / totalStocks) * 100).toFixed(2); 
+            return `${percentage}%`; 
+          }
+        }
+      }
+    },
+  };
+
+  return (
+    <div className="portfolio-composition-chart" style={{position: 'relative', width: '310px', height: '310px' }}>
+      <h3 className="chart-header">Most Traded Issuers</h3>
+      <Pie data={chartData} options={options} /> 
+    </div>
+  );
+}
+
+export default PortfolioCompositionChart;
---
+++ client/src/charts/sectorActivity.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
+
+// Register Chart.js components
+ChartJS.register(ArcElement, Tooltip, Legend, Title);
+
+function SectorActivityChart({ data }) {
+  const totalStocks = data.reduce((total, item) => total + item.count, 0);
+
+  // chart data formatting
+  const chartData = {
+    labels: data.map((item) => item.sector), 
+    datasets: [
+      {
+        label: "Sector Activity",
+        data: data.map((item) => item.count), 
+        backgroundColor: [
+          "#893fe2",
+          "#ec3aa6",
+          "#fee131",
+          "#12adf9",
+          "#20f6c2",
+        ],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      
+      legend: {
+        display: true, 
+        position: "right", 
+        labels: {
+          color: "white", 
+          boxWidth: 15, 
+          padding: 10, 
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            const item = data[tooltipItem.dataIndex];
+            const percentage = ((item.count / totalStocks) * 100).toFixed(2);
+            return `${percentage}%`; 
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="sector-activity-chart" style={{ position: 'relative', width: '400px', height: '400px' }}>
+      <h3 className="chart-header">Most Traded Sectors</h3>
+      <Doughnut data={chartData} options={options} />
+    </div>
+  );
+}
+
+export default SectorActivityChart;
